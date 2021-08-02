811837f46d4d0611902da7662b3e800d46f45e5e
Jörgen Sörling <jorgen@sorling.eu>
2021-08-02 12:24:06 +0200

    Alpine .net setup

---
+++ alpine/machine/setup-aspnetcore.sh
@@ -0,0 +1,27 @@
+#!/bin/ash
+#https://raw.githubusercontent.com/jsorling/miscscripts/main/alpine/buildinfo.ps1
+#Target directory for .Net
+[ ! -d /opt/dotnet ] && mkdir /opt/dotnet
+
+apk add --no-cache wget
+
+#Install reuired packages for .Net
+apk add --no-cache \
+	icu-libs \
+	krb5-libs \
+	libgcc \
+	libintl \
+	libssl1.1 \
+	libstdc++ \
+	zlib
+
+#Fetch and extract latest .Net sdk
+#https://github.com/dotnet/arcade/issues/5757#issue-653403623
+#wget https://aka.ms/dotnet/5.0/aspnetcore-runtime-linux-musl-x64.tar.gz
+wget https://aka.ms/dotnet/5.0/dotnet-sdk-linux-musl-x64.tar.gz
+	  
+tar xvzf dotnet-sdk-linux-musl-x64.tar.gz -C /opt/dotnet
+rm dotnet-sdk-linux-musl-x64.tar.gz
+
+#Create symolic link
+ln -s /opt/dotnet/dotnet /usr/bin/dotnet
