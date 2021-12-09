f366e763b905c171a81c0423d11b2e61675ddbbb
Jörgen Sörling <jorgen@sorling.eu>
2021-12-10 00:40:43 +0100

    pinglog

---
+++ network/pinglog.ps1
@@ -0,0 +1,17 @@
+﻿$target = "192.168.1.150"
+$errorfile = "C:\pingerror.txt"
+
+while($true){
+    $timestamp = (Get-Date).ToString("yyMMdd HH:mm:ss")
+    Write-Host "$timestamp Pinging target $target..."
+
+    try{
+        Test-Connection $target  -ErrorAction "Stop" #-Quiet
+    }
+    catch {
+        Write-Host "$timestamp Pinging target $target failed."
+        Write-Output "$timestamp Pinging target $target failed." | Out-File -FilePath $errorfile -Append
+    }
+    
+    sleep 15
+}
