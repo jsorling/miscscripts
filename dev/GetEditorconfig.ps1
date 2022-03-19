a458e574282775e4174f66add37680661488531c
jsorling <38293402+jsorling@users.noreply.github.com>
2022-03-19 17:46:15 +0100

    Update GetEditorconfig.ps1

--- dev/GetEditorconfig.ps1
+++ dev/GetEditorconfig.ps1
@@ -1,4 +1,4 @@
 #iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/jsorling/miscscripts/main/dev/GetEditorconfig.ps1'))
 
-[string]$ec = wget -UseBasicParsing -Uri "https://raw.githubusercontent.com/jsorling/miscscripts/main/dev/.editorconfig"
+[string]$ec = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jsorling/miscscripts/main/dev/.editorconfig"
 $ec | Set-Content ".\.editorconfig" -NoNewline
