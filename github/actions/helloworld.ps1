3ca8a3436ccd00a35ebcb634890f8ec41f525230
jsorling <38293402+jsorling@users.noreply.github.com>
2023-09-06 23:05:32 +0200

    Update helloworld.ps1

--- github/actions/helloworld.ps1
+++ github/actions/helloworld.ps1
@@ -1,4 +1,5 @@
 #iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/jsorling/miscscripts/main/github/actions/helloworld.ps1'))
 Write-Host "Hello world"
+#https://docs.github.com/en/actions/learn-github-actions/variables
 Write-Host $env:GITHUB_RUN_NUMBER
 Write-Host "Good bye"
